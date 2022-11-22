8602b7c3c3078799b51fa916542eeeef073e540e
syeda-tasmia1 <113660745+syeda-tasmia1@users.noreply.github.com>
2022-11-22 22:30:14 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+//Question 1:
+
+
+for ( let i = 200; i>= 0; i-= 3) {
+    console.log(i);
+}
+
+//Question 2:
+
+
+let z = 0;
+while (z <=9) { 
+  z++;  
+ console.log(z + 'Tasmia')
+}
+
+//Question 3:
+
+let sum= 1;
+let x = 0;
+while (sum <= 10) {
+sum++;
+if (sum % 2 == 0)
+console.log(sum)
+}
+
+//Question 4:
+
+
+let b = 0;
+let c = 4;
+
+do{
+b++;
+  console.log(b);
+}
+
+while (b <= c);
+
+//Question 5
+
+
+function add (x,y) {
+
+console.log(x+y);      
+}
+
+add(9,9)
+
+
+//Question 6
+
+function  box(width,height){
+
+box = width * height;
+
+return console.log(box);
+
+}
+  
+box(10,12);    
